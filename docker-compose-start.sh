b3bca826db16dcc735076f3aa02dadf478277127
ivan <i.gribanov@profinansy.net>
2026-01-27 17:50:40 +0300

    WHOOP-24 review edits (remove scripts, split docker-compose files)

--- docker-compose-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-COMPOSE_FILE="docker-compose.yml"
-
-echo "Starting services..."
-docker-compose -f "$COMPOSE_FILE" up -d
---
+++ docker/postgres/init/create-schema.sql
@@ -0,0 +1,2 @@
+CREATE SCHEMA IF NOT EXISTS wa_auth_service_db;
+CREATE SCHEMA IF NOT EXISTS wa_user_service_db;
